cad3c7a360736a896b8a28d6951d4cb455feb5f2
Eclipse <Eclips77@users.noreply.github.com>
2025-05-21 12:15:17 +0300

    Add project files.

---
+++ IdfProject/Entities/Hamas/Terrorist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdfProject.Entities.Hamas
+{
+    internal class Terrorist
+    {
+    }
+}
---
+++ IdfProject/Entities/Idf/StrikeUnits/Artilery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdfProject.Entities.Interfaces;
+
+namespace IdfProject.Entities.Idf.StrikeUnits
+{
+    internal class Artilery : IStrike
+    {
+    }
+}
